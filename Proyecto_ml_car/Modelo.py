d0895b68ed31505dc530b685b9df81f510d208ab
Epinog <125217092+Epinog@users.noreply.github.com>
2023-04-30 14:41:20 -0500

    Subiendo proyecto ml 1

---
+++ Proyecto_ml_car/Modelo.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import joblib
+import sys
+import os
+
+def predict_price(features):
+    #establecemos la ruta del modelo en pkl
+    regressor = joblib.load(os.path.dirname(_file_) + '/Autos_precio.pkl')
+    print(regressor)
+    #cargamos los datos para generar la estructura
+    dataTraining = pd.read_csv('https://raw.githubusercontent.com/albahnsen/MIAD_ML_and_NLP/main/datasets/dataTrain_carListings.zip')
+    #generamos una copia del Df
+    df_train_A =  dataTraining.copy()
+    #creamos variable de inflexión de precio
+    df_train_A['Inflexion'] = 0
+    #creamos variable para fortalecer modelo sobre marcas
+    df_train_A['4wd_awd'] = 0
+    #generamos dummies
+    df_train_A = pd.get_dummies(df_train_A)
+    columnas = df_train_A.columns
+    df1 = df_train_A.iloc[[0],:]
+    df1[df1 > 0]= 0
+    #variables de captura
+    state = 'State_'+features[2]
+    make = 'Make_'+features[3]
+    model = 'Model_'+features[4]
+    df1['Year'] = int(features[0])
+    df1['Mileage'] = int(features[1])
+    df1[state] = int(1)
+    df1[make] = int(1)
+    df1[model] = int(1)
+    #validamos si es 4x4
+    if '4wd' in features[4].lower():
+        df1['4wd_awd'] = 1
+    #validamos el año para la inflexión:
+    if int(features[0]) >=2010 and int(features[0]) <2014:
+        df1['Inflexion'] = 0.5
+    elif int(features[0]) >= 2014:
+        df1['Inflexion'] = 1
+    else:
+        df1['Inflexion'] = 0
+        
+    # Se realiza la predicción
+    predic = regressor.predict(df1.drop('Price', axis=1))[0]
+
+    return predic
+
+#función inicializadora
+if _name_ == "_main_":
+    
+    if len(sys.argv) == 1:
+        print('Missing data, please check Year, Mileage, State and brand of car')
+        
+    else:
+        lst = sys.argv[1]
+
+        predic = predict_price([Year,Mileage,State,Make])
+        
+        print([Year,Mileage,State,Make])
+        print('Probability of price: ', predic)
+
---
+++ Proyecto_ml_car/api.py.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask
+from flask_restx import Api, Resource, fields
+import joblib
+from flask_cors import CORS
+#se carga la función del Modelo.py
+from Modelo import predict_price
+
+app = Flask(_name_)
+CORS(app)  # Enable CORS for all routes and origins
+
+api = Api(
+    app, 
+    version='1.0', 
+    title='API - Model Prediction Price Car',
+    description='Machile Learning Model to predict car prices')
+
+ns = api.namespace('predict', 
+     description='Car price regressor')
+   
+parser = api.parser()
+
+parser.add_argument(
+    'Year', 
+    type=str, 
+    required=True, 
+    help='Model car year', 
+    location='args')
+
+parser.add_argument(
+    'Mileage', 
+    type=str, 
+    required=True, 
+    help='Car mileage', 
+    location='args')
+
+parser.add_argument(
+    'State', 
+    type=str, 
+    required=True, 
+    help='USA State', 
+    location='args')
+
+parser.add_argument(
+    'Make', 
+    type=str, 
+    required=True, 
+    help='Car manufacturer', 
+    location='args')
+
+parser.add_argument(
+    'Model', 
+    type=str, 
+    required=True, 
+    help='Car model', 
+    location='args')
+
+resource_fields = api.model('Resource', {
+    'result': fields.String,
+})
+
+@ns.route('/')
+class CarApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        return {
+         "result": predict_price([args['Year'],args['Mileage'],args['State'],args['Make'],args['Model']])
+        }, 200
+    
+    
+if _name_ == '_main_':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+
